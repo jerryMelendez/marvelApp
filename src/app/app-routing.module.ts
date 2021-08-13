09c59461355d4109a67c8c4379f6deb0a6c2379c
Jerry <mjerrymoises@gmail.com>
2021-08-13 11:47:46 -0600

    header

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -4,7 +4,7 @@ import { PreloadAllModules, RouterModule, Routes } from '@angular/router';
 const routes: Routes = [
   {
     path: '',
-    redirectTo: 'folder/Inbox',
+    redirectTo: 'home',
     pathMatch: 'full'
   },
   {
@@ -14,6 +14,10 @@ const routes: Routes = [
   {
     path: 'login',
     loadChildren: () => import('./pages/login/login.module').then( m => m.LoginPageModule)
+  },
+  {
+    path: 'home',
+    loadChildren: () => import('./home/home.module').then( m => m.HomePageModule)
   }
 ];
 
--- src/app/components/components.module.ts
+++ src/app/components/components.module.ts
@@ -2,13 +2,16 @@ import { NgModule } from "@angular/core";
 import { MenuComponent } from "./menu/menu.component";
 import { CommonModule } from '@angular/common';
 import { IonicModule } from '@ionic/angular';
+import { HeaderComponent } from './header/header.component';
 
 @NgModule({
     declarations: [
-        MenuComponent
+        MenuComponent,
+        HeaderComponent
     ],
     exports: [
-        MenuComponent
+        MenuComponent,
+        HeaderComponent
     ],
     imports: [
         CommonModule,
